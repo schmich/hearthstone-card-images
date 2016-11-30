fd5d2328c76efed667f66c7039df0b147b773804
Chris Schmich <schmch@gmail.com>
2016-11-30 17:15:23 -0600

    Add short URLs to CDN map. Short URLs only use the first 4 characters of the commit hash.

--- create-cdn-map.rb
+++ create-cdn-map.rb
@@ -2,17 +2,27 @@ require 'json'
 
 cards = []
 
+short_revs = `git rev-list master`.lines.map(&:strip).map { |rev| rev[0...4] }
+if short_revs.length != short_revs.uniq.length
+  raise "Short revisions collide, see 'git rev-list master'."
+end
+
 Dir['**/*.png'].each do |file|
   id = File.basename(file, '.png')
   rev = `git rev-list master -1 -- #{file}`.strip
   path = "#{rev}/#{file}"
   url = "https://cdn.rawgit.com/schmich/hearthstone-card-images/#{path}"
 
+  short_rev = rev[0...4]
+  short_path = "#{short_rev}/#{file}"
+  short_url = "https://cdn.rawgit.com/schmich/hearthstone-card-images/#{short_path}"
+
   cards << {
     id: id,
     rev: rev,
     path: path,
-    url: url
+    url: url,
+    shortUrl: short_url
   }
 end
 
