ef52480d3611743cd21450e5e8a5d9e944bd534d
Chris Schmich <schmch@gmail.com>
2018-03-17 00:19:41 -0700

    Add comments to sync.sh.

--- sync.sh
+++ sync.sh
@@ -19,26 +19,30 @@ if [[ ! $REPLY =~ ^[Yy] ]]; then
   exit 1
 fi
 
+# Fetch latest images.json database.
 echo 'Downloading latest images.json.'
 json=$(curl -Ls https://raw.githubusercontent.com/schmich/hearthstone-card-images/master/images.json)
 base=$(jq -r .config.base <<< "$json")
 cards=$(jq -r '.cards | to_entries[] | [.key, .value[0], .value[1]] | @tsv' <<< "$json")
-count=$(wc -l <<< "$cards" | tr -d ' ')
+total=$(wc -l <<< "$cards" | tr -d ' ')
 
 index=0
 while IFS=$'\t' read -r id path hash; do
   ((index++))
-  echo -n "[$index/$count] "
+  echo -n "[$index/$total] "
 
+  # Check if local image exists.
   local_file="$dir/$id.png"
   if [[ -e $local_file ]]; then
     local_hash=$(openssl sha1 -binary "$local_file" | openssl base64 | cut -c1-5)
+    # Check if local image has same hash as remote image.
     if [[ $local_hash == $hash ]]; then
       echo "$local_file is up-to-date."
       continue
     fi
   fi
 
+  # Local image doesn't exist or is different. Update it.
   url="$base$path/$id.png"
   echo "Sync $path/$id.png -> $local_file."
   curl -Lso "$local_file" "$url"
